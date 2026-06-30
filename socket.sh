c076611154d2824cf4a6db86e80a37e5ab58689d
dddd <dddd@gmail.com>
2026-06-30 12:08:37 +0800

    update code

--- socket.sh
+++ socket.sh
@@ -8,6 +8,6 @@ pkill -9 -f "$SERVER_DIR/snb.php" >/dev/null 2>&1
 cd "$SERVER_DIR" || exit 1
 sleep 2
 
-nohup "$PHP_BIN" "$SERVER_DIR/snb.php" 127.0.0.1 11211 1988 127.0.0.1 kdjl 0 >/dev/null 2>&1 &
+nohup su -s /bin/sh www -c "$PHP_BIN $SERVER_DIR/snb.php 127.0.0.1 11211 1988 127.0.0.1 kdjl 0" >/dev/null 2>&1 &
 
 exit 0
